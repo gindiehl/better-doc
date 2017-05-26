b0fb381e6d7e56722d488b1a8473a6fe6edaf286
jin camou <jincamou@gmail.com>
2017-05-26 15:00:17 -0700

    add jshintrc file with updated esversion, return object

--- js/better-doc-interface.js
+++ js/better-doc-interface.js
@@ -1,16 +1,16 @@
 var Doctor = require('./../js/better-doc.js').doctorModule;
 
-var displayDoctors = function(response) {
-  response.practices.forEach(function(doctor) {
-    $('.showDocs').append(`<li>Location:${practice.location_slug}</li>`)
-  });
-};
+// var displayDoctors = function(response) {
+//   response.practices.forEach(function(practice) {
+//     $('.showDocs').append(`<li>Doctor:${practice.name}</li>`);
+//   });
+// };
 
 $(document).ready(function() {
   $('#locate').click(function() {
     var currentDoctorObject = new Doctor();
     var city = $("#city").val();
     var symptom = $("#symptom").val();
-    currentDoctorObject.getDoctors(city, displayDoctors);
+    currentDoctorObject.getDoctor(city, symptom);
   });
 });
