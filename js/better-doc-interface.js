b23a25fc2b1b4eea254fca8dcea044678d11b4d3
jin camou <jincamou@gmail.com>
2017-05-26 16:21:18 -0700

    target data and run forEach loop to show names of doctors

--- js/better-doc-interface.js
+++ js/better-doc-interface.js
@@ -1,16 +1,17 @@
 var Doctor = require('./../js/better-doc.js').doctorModule;
 
-// var displayDoctors = function(response) {
-//   response.practices.forEach(function(practice) {
-//     $('.showDocs').append(`<li>Doctor:${practice.name}</li>`);
-//   });
-// };
+var displayDoctors = function(response) {
+  response.data.forEach(function(doctor) {
+    $('.showDocs').append(`<li>Doctor: ${doctor.practices[0].name}</li>`);
+
+  });
+};
 
 $(document).ready(function() {
   $('#locate').click(function() {
     var currentDoctorObject = new Doctor();
     var city = $("#city").val();
     var symptom = $("#symptom").val();
-    currentDoctorObject.getDoctor(city, symptom);
+    currentDoctorObject.getDoctor(city, symptom, displayDoctors);
   });
 });
--- js/better-doc.js
+++ js/better-doc.js
@@ -3,12 +3,10 @@ var apiKey = require('./../.env').apiKey;
 Doctor = function(){
 }
 
-Doctor.prototype.getDoctor = function(city, symptom) {
-  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query=' + city + '&skip=0&limit=10&user_key=' + apiKey)
-    .then(function(response) {
-      // displayDoctors(response)
-      console.log(response);
-    $('.showDocs').text(response);
+Doctor.prototype.getDoctor = function(city, symptom, displayDoctors) {
+  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query=' + city + symptom + '&skip=0&limit=25&user_key=' + apiKey).then(function(response) {
+    console.log(response);
+    displayDoctors(response)
   })
   .fail(function(error) {
     $('.showDocs').text(error.responseJSON.message);
